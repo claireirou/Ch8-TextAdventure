1a7f56ecd9d331c3e6344250b306f5b44cc33f23
Claire Irou <c.iroudayassamy@gmail.com>
2019-04-15 01:42:00 -0400

    Oh, wait.
    Made game2 class the offical game and renamed it game.
    Fixed some teeny tiny things I missed.
    Now it's bedtime. I'm a zombie.

--- CommandWords.java
+++ CommandWords.java
@@ -65,7 +65,7 @@ public class CommandWords
     public void showAll() 
     {
         for(String command : validCommands.keySet()) {
-            System.out.print(command + " -");
+            System.out.print(command + " - ");
         }
         System.out.println();
     }
